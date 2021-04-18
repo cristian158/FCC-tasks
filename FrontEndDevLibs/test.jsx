dedc719f4854b3b888c3db16a3c8f4302fbe18b7
cristian158 <cnovoa.o@gmail.com>
2021-04-18 17:48:13 -0400

    here we go (Debian)

--- FrontEndDevLibs/test.jsx
+++ FrontEndDevLibs/test.jsx
@@ -1,7 +1,10 @@
 const JSX = (
-  <div>
-    <h1>Hello World</h1>
+  <div className='myDiv'>
+  {/* key difference JSX vs HTML: 'class' is reserved word in JavaScript. JSX uses className. */}
+    <h1>Hello World</h1> <br />
     <p>Lets render this to the DOM</p>
+    <hr />
+    {/*all tags must be closed, even self-closing like br and hr*/}
   </div>
 );
 {/*Change code below this line*/}
